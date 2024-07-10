b13f26a0c07d6ca6d2e4f67bd3f2c46fa7f0a24e
Heather Smith <64465757+Void-Stag@users.noreply.github.com>
2024-07-10 16:55:49 +0100

    Forth half of finalised system

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P2659902___Library_System.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(P2659902___Library_System.Startup))]
+namespace P2659902___Library_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
